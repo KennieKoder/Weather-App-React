d6462e3f171da014af41152fc8cddd5150824f52
KennieKoder <makenahdumont99@gmail.com>
2023-02-27 16:46:14 -0500

    minor change

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="Footer">
       <span>Built with React by Makenah Dumont</span>
-      <span>See on <a href="https://github.com/KennieKoder/Weather-App-React" target="_blank" rel="noreferrer">GitHub</a></span>
+      <span> See on <a href="https://github.com/KennieKoder/Weather-App-React" target="_blank" rel="noreferrer">GitHub</a></span>
     </div>
   );
 }
